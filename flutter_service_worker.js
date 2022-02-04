a1186d63057a6c53b620d6edd6dcf8a10a18476c
Nilesh Payghan <51196316+NileshTech@users.noreply.github.com>
2022-02-04 12:52:43 +0530

    user data in shared prefs handled

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "295e26eceb35ca1d77ac7de3c97538e9",
-"index.html": "f52be52dbcc1dfd46b3ab153e20b2629",
-"/": "f52be52dbcc1dfd46b3ab153e20b2629",
+"index.html": "b0965ebadabde6794820cfff7d18743f",
+"/": "b0965ebadabde6794820cfff7d18743f",
 "firebase-messaging-sw.js": "2cf40a461f2d10f7ce15517dcc70a80d",
-"main.dart.js": "d0c3fb26a575537b3d9e6d50966b5caf",
+"main.dart.js": "c0c9b7a8db6953f0ac93804a0a39aee9",
 "favicon.png": "3647872be98fa436e090a37ba2dd1852",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
