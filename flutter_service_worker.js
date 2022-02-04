85c701c627ea6ac69962a8860dd01666874ebb20
Nilesh Payghan <51196316+NileshTech@users.noreply.github.com>
2022-02-04 11:48:45 +0530

    deep link handling

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "295e26eceb35ca1d77ac7de3c97538e9",
-"index.html": "525cfda4b663b29b2ad54ca6764fe07e",
-"/": "525cfda4b663b29b2ad54ca6764fe07e",
+"index.html": "f1c1ff59bf367365be52fae42310b4fa",
+"/": "f1c1ff59bf367365be52fae42310b4fa",
 "firebase-messaging-sw.js": "2cf40a461f2d10f7ce15517dcc70a80d",
-"main.dart.js": "ed7ad68f04bcf49f424736e0a74238b1",
+"main.dart.js": "666e294a482879993739e2fe7f74c075",
 "favicon.png": "3647872be98fa436e090a37ba2dd1852",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
