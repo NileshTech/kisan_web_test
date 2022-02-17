f4b4ff98e7145481c49b4bcca788579d6465f239
Nilesh Payghan <51196316+NileshTech@users.noreply.github.com>
2022-02-17 11:17:09 +0530

    2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "295e26eceb35ca1d77ac7de3c97538e9",
-"index.html": "34d87324b98f9bbd5da3348a0e9abc95",
-"/": "34d87324b98f9bbd5da3348a0e9abc95",
+"index.html": "3711203124b7ec81f44370adf919b77b",
+"/": "3711203124b7ec81f44370adf919b77b",
 "firebase-messaging-sw.js": "2cf40a461f2d10f7ce15517dcc70a80d",
-"main.dart.js": "fd5c0be2d9fcf13b5064852b127b38c9",
+"main.dart.js": "97df7ac058e36cdb52ed7c7d5a57913e",
 "favicon.png": "3647872be98fa436e090a37ba2dd1852",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
